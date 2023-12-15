fbc1da501bbb023e7a96c178630094c03b37dd0c
Shahne-Alam <153852780+Shahne-Alam@users.noreply.github.com>
2023-12-15 19:35:49 +0000

    data types changes done

---
+++ 01_basices/02_Data_Types.js
@@ -0,0 +1,15 @@
+let name = "Shahne Alam"
+let age = 20;
+let isLoggedId = false;
+let state;
+
+//number=> 2 to power 53
+//string=> ""
+//BigInt
+//Boolean=> true/false
+//null=> standalone value
+//undefined
+//Symbol=> unique
+//Object
+
+console.log(typeof "");
