ec22ff41cc9687564e015cceb906ce824f42f35a
Shahne-Alam <153852780+Shahne-Alam@users.noreply.github.com>
2023-12-15 18:09:57 +0000

    variables changes

--- 01_basices/01_variables.js
+++ 01_basices/01_variables.js
@@ -1,7 +1,7 @@
 const accountId = 14455;
 let accountEmail = "shahne@gmail.com";
 var accountPassword = 12345;
-accountCity = "Sambhal";
+let accountCity = "Sambhal";
 let accountState;
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
