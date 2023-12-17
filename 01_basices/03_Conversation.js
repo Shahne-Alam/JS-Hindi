32633d5336aba41af2e4ab08194b15a7ff8c3a70
Shahne-Alam <153852780+Shahne-Alam@users.noreply.github.com>
2023-12-17 19:35:14 +0000

    done

---
+++ 01_basices/03_Conversation.js
@@ -0,0 +1,24 @@
+let score = "338"
+
+console.log(typeof score);
+
+let change = Number(score);
+console.log(change);
+console.log(typeof change)
+let shahne = null;
+console.log(typeof shahne);
+let alam = NaN;
+console.log(typeof alam);
+let bro = 8 + '8';
+console.log(bro);
+
+let ary = [1,2,3,4,5,6,7];
+console.log( typeof ary);
+
+// let tinku = {
+//     Name:"Shahne",
+//     Age: 18,
+//     State: "UP"
+// }
+
+// console.log( typeof tinku);
