88690e0e950423fcb7c707ffb39f9b7af82d4643
Shahne-Alam <153852780+Shahne-Alam@users.noreply.github.com>
2023-12-18 18:35:06 +0000

    done

--- 01_basices/03_Conversation.js
+++ 01_basices/03_Conversation.js
@@ -1,19 +1,19 @@
 let score = "338"
 
-console.log(typeof score);
+// console.log(typeof score);
 
 let change = Number(score);
-console.log(change);
-console.log(typeof change)
-let shahne = null;
+// console.log(change);
+// console.log(typeof change)
+// let shahne = null;
 console.log(typeof shahne);
 let alam = NaN;
-console.log(typeof alam);
+// console.log(typeof alam);
 let bro = 8 + '8';
-console.log(bro);
+// console.log(bro);
 
 let ary = [1,2,3,4,5,6,7];
-console.log( typeof ary);
+// console.log( typeof ary);
 
 // let tinku = {
 //     Name:"Shahne",
@@ -22,3 +22,6 @@ console.log( typeof ary);
 // }
 
 // console.log( typeof tinku);
+
+/*************************Operations ********************** */
+
