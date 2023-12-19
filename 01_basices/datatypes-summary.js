5ee56a6d2b4ec02efec2f34f1ce488d595330fa2
Shahne-Alam <153852780+Shahne-Alam@users.noreply.github.com>
2023-12-19 16:27:36 +0000

    done

--- 01_basices/datatypes-summary.js
+++ 01_basices/datatypes-summary.js
@@ -3,10 +3,25 @@
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt
 
 const outSide = null;
-console.log(typeof outSide)
+const bigNumber = 234354623;
+const Id = "shahnealam.com";
+
+console.log(typeof Id)
 
 
 
 // Reference (Non Primitive)
 
 // Array, Object, Function
+
+const hero = ['hero', 'alam', 'shahne', 'shan'];
+let heroobj = {
+    name: "shahne",
+    age: 20,
+    class: "B.com"
+}
+
+const myFunction = function () {
+    console.log('myFunction');
+}
+console.log(typeof myFunction)
