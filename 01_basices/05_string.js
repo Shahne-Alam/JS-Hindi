478aeb965ac7a36701709506fc37c5f7c78099bd
Shahne-Alam <153852780+Shahne-Alam@users.noreply.github.com>
2023-12-20 19:14:56 +0000

    node

--- 01_basices/05_string.js
+++ 01_basices/05_string.js
@@ -1,3 +1,5 @@
+// First Way
+
 const Name = "Shahne" + "Alam"
 console.log(Name);
 
@@ -5,3 +7,10 @@ console.log(Name);
 
  Fname = "Shahne" + "Alam"
  console.log(`My Name is ${Fname}`);
+
+const gameName = new String('shahne Alam')
+// console.log(gameName [0]);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+console.log(gameName.indexOf('n'));
