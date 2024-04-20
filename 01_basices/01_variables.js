b4b4d7c1098123f5669892b33d3e2ac6ba366a1f
Shahne Alam <153852780+Shahne-Alam@users.noreply.github.com>
2024-04-20 07:32:30 +0000

    value change

--- 01_basices/01_variables.js
+++ 01_basices/01_variables.js
@@ -2,7 +2,7 @@ const accountId = 14455;
 let accountEmail = "shahne@gmail.com";
 var accountPassword = 12345;
 let accountCity = "Sambhal";
-let accountState;
+let accountState = 'UP';
 
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 
